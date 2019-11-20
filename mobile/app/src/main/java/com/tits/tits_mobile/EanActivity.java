15cbb547bedc6a23f9c6ad498086f90b76857e6d
norbertpaluczynski <norbert.paluczynski@gmail.com>
2019-11-20 19:35:21 +0100

    Added http request

--- mobile/app/src/main/java/com/tits/tits_mobile/EanActivity.java
+++ mobile/app/src/main/java/com/tits/tits_mobile/EanActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.concurrent.ExecutionException;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EanActivity extends AppCompatActivity {
@@ -24,7 +26,20 @@ public class EanActivity extends AppCompatActivity {
 
         if (getIntent().getStringExtra("EAN") != null) {
             EAN = getIntent().getStringExtra("EAN");
-            eanTxtView.setText(EAN);
+
+            String myUrl = "http://217.182.79.249/api/products/getByEan?ean=" + EAN;   //String to place our result in
+            String result;
+            HttpGetRequest getRequest = new HttpGetRequest();
+            try {
+                result = getRequest.execute(myUrl).get();
+                System.out.println(result);
+                eanTxtView.setText(result);
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
         }
 
         anotherScan.setOnClickListener(new View.OnClickListener() {
--- mobile/app/src/main/java/com/tits/tits_mobile/MainActivity.java
+++ mobile/app/src/main/java/com/tits/tits_mobile/MainActivity.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutionException;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnScanBarcode;
+    Button btnEan;
+    TextView eanEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +33,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnScanBarcode = findViewById(R.id.btnScanBarcode);
+        btnEan = findViewById(R.id.eanTextButton);
+        eanEditText = findViewById(R.id.eanEditText);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        btnEan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EanActivity.class).putExtra("EAN", eanEditText.getText().toString()));
+            }
+        });
 
         btnScanBarcode.setOnClickListener(new View.OnClickListener() {
             @Override
