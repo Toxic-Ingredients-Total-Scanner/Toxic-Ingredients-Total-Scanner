b9be82a4614ac1cbfd65ada354548788fe479a8b
Kacper Wleklak <kacper.wleklak@gmail.com>
2019-11-22 00:35:26 +0100

    [front] cleanup project and example components

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,13 +2,23 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify-css');
+var merge = require('merge-stream');
 
 //tasks
 function style() {
-    return gulp.src('./app/less/**/*.less')
-        .pipe(less())
-        .pipe(gulp.dest('./app/css'))
-        .pipe(browserSync.stream());
+  var lessStream = gulp.src('./app/**/*.less')
+    .pipe(less())
+    .pipe(concat('less-files.less'))
+  ;
+  var mergedStream = merge(lessStream)
+    .pipe(concat('style.css'))
+    //.pipe(minify())
+    .pipe(gulp.dest('./app'));
+
+  return mergedStream;
 }
 
 function watch() {
@@ -18,7 +28,7 @@ function watch() {
         },
         port: 8000
     });
-    gulp.watch('./app/less/**/*.less', style);
+    gulp.watch('./app/**/*.less', style);
     gulp.watch('./**/*.html').on('change', browserSync.reload);
     gulp.watch('./**/*.js').on('change', browserSync.reload);
 }
