8d4b31adb358381ebc14e215ef6a677f74b06bb2
Tarnacki <tarnac97@gmail.com>
2019-11-23 16:57:39 +0100

    GHS Statements method

--- api/TITS_API.Services/Services/PubChemService.cs
+++ api/TITS_API.Services/Services/PubChemService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
@@ -21,6 +22,7 @@ namespace TITS_API.Services.Services
         private const string apiUrl = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/";
         private const string baseUrl = "https://pubchem.ncbi.nlm.nih.gov/compound/";
         private const string autoCompleteUrl = "https://pubchem.ncbi.nlm.nih.gov/rest/autocomplete/compound/";
+        private const string informationUrl = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/";
 
         public PubChemService(TranslateService translateService)
         {
@@ -40,24 +42,8 @@ namespace TITS_API.Services.Services
             ingredient.MolecularFormula = (await _http.GetStringAsync(apiUrl + "compound/cid/" + ingredient.PubChemCID + "/property/MolecularFormula/TXT")).Trim();
             ingredient.StructureImageUrl = apiUrl + "compound/cid/" + ingredient.PubChemCID + "/PNG";
             ingredient.GHSClasificationRaportUrl = ingredient.PubChemUrl + "#datasheet=LCSS&section=GHS-Classification&fullscreen=true";
-
-            var wikiResponse = await _http.GetStringAsync("https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/" + ingredient.PubChemCID + "/XML");
-
-            if (!wikiResponse.Contains("PUGVIEW.NotFound"))
-            {
-                XmlDocument doc = new XmlDocument();
-                doc.LoadXml(wikiResponse);
-
-                XmlNodeList elemList = doc.GetElementsByTagName("URL");
-                foreach (XmlNode elem in elemList)
-                {
-                    if (elem.InnerText.Contains("https://en.wikipedia.org/wiki/"))
-                    {
-                        ingredient.WikiUrl = elem.InnerXml;
-                        break;
-                    }
-                }
-            }
+            ingredient.WikiUrl = await WikipediaURL(ingredient);
+             
 
             return ingredient;
         }
@@ -73,5 +59,64 @@ namespace TITS_API.Services.Services
 
             return null;
         }
+
+        private async Task<string> WikipediaURL(Ingredient ingredient)
+        {
+            string wikiUrl = "";
+
+            var wikiResponse = await _http.GetStringAsync(informationUrl + ingredient.PubChemCID + "/XML");
+
+            if (!wikiResponse.Contains("PUGVIEW.NotFound"))
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(wikiResponse);
+
+                XmlNodeList nodes = doc.GetElementsByTagName("URL");
+                foreach (XmlNode node in nodes)
+                {
+                    if (node.InnerText.Contains("https://en.wikipedia.org/wiki/"))
+                    {
+                        wikiUrl = node.InnerXml;
+                        break;
+                    }
+                }
+            }
+
+            return wikiUrl;
+        }
+
+        private async Task<string[]> GHSStatements(Ingredient ingredient)
+        {
+            string[] GHSStatemments;
+            string temp = "";
+            var GHSResponse = await _http.GetStringAsync(informationUrl + ingredient.PubChemCID + "/XML");
+            Regex regex = new Regex(@"(H\d{3}([a-z]|[A-Z]){0,2}( |\:))|(EUH\d{3}( |\:))|(AUH\d{3})( |\:)");
+
+            if (!GHSResponse.Contains("PUGVIEW.NotFound"))
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(GHSResponse);
+
+                XmlNodeList nodes = doc.GetElementsByTagName("String");
+                foreach (XmlNode node in nodes)
+                {
+                    Match match = regex.Match(node.InnerXml);
+                    if (match.Success)
+                    {
+                        String[] splitTable = node.InnerXml.Split(new char[] {' ', ':'});
+                        if (!temp.Contains(splitTable[0]))
+                        {
+                            temp += splitTable[0] + ' ';
+                        }
+                    }
+                }
+            }
+
+            temp = temp.Trim();
+            GHSStatemments = temp.Split(' ');
+
+            return GHSStatemments;
+        }
+
     }
 }
