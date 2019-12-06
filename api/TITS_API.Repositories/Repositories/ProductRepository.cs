4297f5b8ceabc000ca20112f80c60551b40348fe
michalratajczak2105 <michalratajczak2105@gmail.com>
2019-12-06 15:42:46 +0100

    [api] Add method which allows to get products names hints. (fix 5)

--- api/TITS_API.Repositories/Repositories/ProductRepository.cs
+++ api/TITS_API.Repositories/Repositories/ProductRepository.cs
@@ -7,6 +7,7 @@ using TITS_API.Models.Models;
 using TITS_API.Repositories.Interfaces;
 using System.Linq;
 
+
 namespace TITS_API.Repositories.Repositories
 {
     public class ProductRepository : Repository<Product, DatabaseContext>
@@ -26,13 +27,53 @@ namespace TITS_API.Repositories.Repositories
 
         public async Task<List<ProductHint>> GetProductNames(string name)
         {
-            var words = name.Split().ToList();
-            return await Task.Run(() =>
-            _context.Products.AsEnumerable().Where(p => words.All(w => p.ProductName.ToUpper().Contains(w.ToUpper()))).Select(p =>
-            new ProductHint
-            { ProductName = p.ProductName,
-                Gtin = p.Gtin
-            }).Take(10).ToList());
+            const int take = 10;
+            var words = name.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            switch (words.Count)
+            {
+                case 0: 
+                    return null;
+                case 1:
+                    return await Task.Run(() => _context.Products.Where(p => 
+                        p.ProductName.ToUpper().Contains(words[0].ToUpper()))
+                        .Select(p => new ProductHint{ ProductName = p.ProductName, Gtin = p.Gtin }).Take(take).ToList());
+                case 2:
+                    return await Task.Run(() => _context.Products.Where(p =>
+                        p.ProductName.ToUpper().Contains(words[0].ToUpper()) &&
+                        p.ProductName.ToUpper().Contains(words[1].ToUpper()))
+                        .Select(p => new ProductHint { ProductName = p.ProductName, Gtin = p.Gtin }).Take(take).ToList());
+                case 3:
+                    return await Task.Run(() => _context.Products.Where(p =>
+                        p.ProductName.ToUpper().Contains(words[0].ToUpper()) &&
+                        p.ProductName.ToUpper().Contains(words[1].ToUpper()) &&
+                        p.ProductName.ToUpper().Contains(words[2].ToUpper()))
+                        .Select(p => new ProductHint { ProductName = p.ProductName, Gtin = p.Gtin }).Take(take).ToList());
+                case 4:
+                    return await Task.Run(() => _context.Products.Where(p =>
+                        p.ProductName.ToUpper().Contains(words[0].ToUpper()) &&
+                        p.ProductName.ToUpper().Contains(words[1].ToUpper()) &&
+                        p.ProductName.ToUpper().Contains(words[2].ToUpper()) &&
+                        p.ProductName.ToUpper().Contains(words[3].ToUpper()))
+                        .Select(p => new ProductHint { ProductName = p.ProductName, Gtin = p.Gtin }).Take(take).ToList());
+                case 5:
+                    return await Task.Run(() => _context.Products.Where(p =>
+                        p.ProductName.ToUpper().Contains(words[0].ToUpper()) &&
+                        p.ProductName.ToUpper().Contains(words[1].ToUpper()) &&
+                        p.ProductName.ToUpper().Contains(words[2].ToUpper()) &&
+                        p.ProductName.ToUpper().Contains(words[3].ToUpper()) &&
+                        p.ProductName.ToUpper().Contains(words[4].ToUpper()))
+                        .Select(p => new ProductHint { ProductName = p.ProductName, Gtin = p.Gtin }).Take(take).ToList());
+                default:
+                    return await Task.Run(() => _context.Products.Where(p =>
+                        p.ProductName.ToUpper().Contains(words[0].ToUpper()) &&
+                        p.ProductName.ToUpper().Contains(words[1].ToUpper()) &&
+                        p.ProductName.ToUpper().Contains(words[2].ToUpper()) &&
+                        p.ProductName.ToUpper().Contains(words[3].ToUpper()) &&
+                        p.ProductName.ToUpper().Contains(words[4].ToUpper()) &&
+                        p.ProductName.ToUpper().Contains(words[5].ToUpper()))
+                        .Select(p => new ProductHint { ProductName = p.ProductName, Gtin = p.Gtin }).Take(take).ToList());
+            }
         }
 
     }
