c87b15e5a3d76ac31db4f1f13fa4e039b87a1e18
norbertpaluczynski <norbert.paluczynski@gmail.com>
2019-12-08 00:57:52 +0100

    [mobile] Added autocomplete ingredients on editProduct

--- mobile/app/src/main/java/com/tits/tits_mobile/editProduct.java
+++ mobile/app/src/main/java/com/tits/tits_mobile/editProduct.java
@@ -2,26 +2,27 @@ package com.tits.tits_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tits.tits_mobile.HttpHandler.HttpGetRequest;
 import com.tits.tits_mobile.HttpHandler.HttpPutRequest;
 import com.tits.tits_mobile.models.Ingredient;
 import com.tits.tits_mobile.models.Product;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
 public class editProduct extends AppCompatActivity {
 
@@ -31,20 +32,16 @@ public class editProduct extends AppCompatActivity {
     EditText eanEditText;
     EditText brandEditText;
     EditText productNameEditText;
-    EditText ingredientsEditText;
+    AutoCompleteTextView ingredientsEditText;
     ListView list;
     Button sendBtn;
     Button confirmIng;
     String json;
+    ArrayList<String> autocomplete;
+    String result;
+    ArrayList<String> reqHints;
+    String[] reqHintsArr;
 
-    public Ingredient findIngredientByName(String name) {
-        for(Ingredient ingredientObj : ingList) {
-            if(ingredientObj.getEnglishName().equals(name)) {
-                return ingredientObj;
-            }
-        }
-        return null;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,22 +60,43 @@ public class editProduct extends AppCompatActivity {
 
 
         ingStrings = new ArrayList<>();
+        autocomplete = new ArrayList<>();
+        reqHints = new ArrayList<>();
+        reqHintsArr = new String[]{};
 
-        prod = (Product) getIntent().getExtras().getSerializable("prod");
-        ingList = prod.getIngredients();
+        final String myUrl = "http://vps756014.ovh.net/api/Ingredients/names";
+
+        prod = (Product) Objects.requireNonNull(getIntent().getExtras()).getSerializable("prod");
+        if (prod != null) {
+            ingList = prod.getIngredients();
+        }
 
         eanEditText.setText(prod.getGtin());
         brandEditText.setText(prod.getBrand());
         productNameEditText.setText(prod.getProductName());
+
+        HttpGetRequest getRequest = new HttpGetRequest(editProduct.this);
+        try {
+            result = getRequest.execute(myUrl).get();
+            ObjectMapper objectMapper = new ObjectMapper();
+            reqHints = objectMapper.readValue(result,objectMapper.getTypeFactory().constructCollectionType(List.class, String.class));
+            System.out.println(reqHints.size());
+        } catch (ExecutionException | InterruptedException | JsonProcessingException e) {
+            e.printStackTrace();
+        }
 
         for(Ingredient i : ingList){
             ingStrings.add(i.getPolishName());
             //System.out.println(i.getPolishName());
         }
 
+        final ArrayAdapter<String> hintAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, reqHints.toArray(new String[0]));
+
+        ingredientsEditText.setAdapter(hintAdapter);
 
 
-        final ArrayAdapter adapter = new ArrayAdapter<String>(
+        final ArrayAdapter adapter = new ArrayAdapter<>(
                 this, R.layout.list_item_edit, R.id.ingName, ingStrings
         );
 
@@ -89,7 +107,6 @@ public class editProduct extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String item = ingStrings.get(position);
-                Ingredient ingr = findIngredientByName(item);
 
                 ingStrings.remove(item);
                 ingList.remove(ingList.get(position));
